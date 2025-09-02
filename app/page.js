7bf734d9c992ef23d06f7bc4b2c0687f3ed7e51d
Sarra72 <sarahmedd72@gmail.com>
2025-09-03 01:04:29 +0300

    final header and footer

--- app/page.js
+++ app/page.js
@@ -3,8 +3,8 @@ export default function Home() {
   return (
     <>
       <div>
-        <h1 className="p-[100px]">
-          main
+        <h1 className="p-[100px] text-center">
+          Home
         </h1>
       </div>
     </>
