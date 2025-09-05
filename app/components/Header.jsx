c17eff047dd057ff1ad6c86aa3d5e5874effb27f
Sarra72 <sarahmedd72@gmail.com>
2025-09-06 00:09:04 +0300

    total products added to header

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -109,7 +109,7 @@ return (
                     <Image src="/assets/menuIcon.svg" alt="menu icon" height={14} width={14} className="max-md:hidden" />
                     <span className={`category-btn-txt ${dosis.className}`}> All categories </span>
                     <Image src="/assets/dropArrow.svg" height={10} width={10} alt="drop arrow" className={`transition-transform duration-300 ${ categoriesOpen ? "rotate-180" : "rotate-0"}`} />
-                    
+                    <span className={`total-product ${dosis.className} `}>total 50 Products</span>
                 </button>
 
                 {/* Dropdown */}
